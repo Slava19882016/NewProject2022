3541286759809336060d0330cad4def624bde8ce
Slava <vp1891719@gmail.com>
2022-06-03 16:57:37 +0300

    Function2

--- JS/script.js
+++ JS/script.js
@@ -1,67 +1,78 @@
 'use strict';
 
-let numberOfFilms; 
 
-function start() {
-    numberOfFilms = +prompt ("Сколько фильмов Вы уже посмотрели?", "");
-
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt ("Сколько фильмов Вы уже посмотрели?", "");
-    }
-} 
-start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt ("Сколько фильмов Вы уже посмотрели?", "");
+    
+        while (personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt ("Сколько фильмов Вы уже посмотрели?", "");
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt ("Один из последних просмотренных фильмов?", ""),
+                  b = prompt ("На сколько Вы его оцениваете?", "");
+        
+            if (a != null && b != null && a !== "" && b != "" && a < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log("done");
+            } else {
+                console.log("error");
+                i--;
+            }      
+        }
+    },
+    detectPersonalLevel:function() {
+        if (personalMovieDB.count < 20) {
+            console.log("Вы просмотрели очень мало фильмов");
+        } else if (personalMovieDB.count >=20 && personalMovieDB.count <50) {
+            console.log("Вы классический любитель кино");
+        } else if (personalMovieDB.count >= 50) {
+            console.log("Вы тратите слишком много времени на кино");
+        } else {
+            console.log("error");
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt (`Ваш любимый жанр под номером ${i}`);
+
+            if (genre === "" || genre == null) {
+                console.log("Вы ввели не корректные данные");
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
+    } 
 };
 
 
-function rememberMyFilms () {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt ("Один из последних просмотренных фильмов?", ""),
-              b = prompt ("На сколько Вы его оцениваете?", "");
-    
-        if (a != null && b != null && a !== "" && b != "" && a < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log("done");
-        } else {
-            console.log("error");
-            i--;
-        }      
-    }
-}
-// rememberMyFilms();
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 20) {
-        console.log("Вы просмотрели очень мало фильмов");
-    } else if (personalMovieDB.count >=20 && personalMovieDB.count <50) {
-        console.log("Вы классический любитель кино");
-    } else if (personalMovieDB.count >= 50) {
-        console.log("Вы тратите слишком много времени на кино");
-    } else {
-        console.log("error");
-    };
-}
-// detectPersonalLevel();
 
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt (`Ваш любимый жанр под номером ${i}`);
-    }
-}
-writeYourGenres();
 
 
 
